21024e38e11ac42a0ea0486719ba732c86c66841
ramu siripalli <ramsiripalli2772@gmail.com>
2025-12-19 20:04:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
+function mincost(arr) {
+  if (arr.length <= 1) return 0;
+
+  let cost = 0;
+
+  while (arr.length > 1) {
+    // sort array in ascending order
+    arr.sort((a, b) => a - b);
+
+    // pick two smallest ropes
+    let first = arr.shift();
+    let second = arr.shift();
+
+    let sum = first + second;
+    cost += sum;
+
+    // push the combined rope back
+    arr.push(sum);
+  }
+
+  return cost;
 }
 
-module.exports=mincost;
+module.exports = mincost;
